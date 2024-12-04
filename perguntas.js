b944e79673d273b990fa7c5067c53c1a9fc5a4f3
ViNaum56 <00001102007766sp@al.educacao.sp.gov.br>
2024-12-04 15:06:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'O que é o Javascript?',
+    'O Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de programação'
+)
